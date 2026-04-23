113b4c9f554a932900c62d1cb2284519de466698
Gabriela Angel Silva <90701152+gabriela-angel@users.noreply.github.com>
2026-04-23 17:56:30 -0300

    corrige funcao do calculo da derivada
    Removed commented-out code for derivative calculation and fixed variable assignments.

--- src/main.c
+++ src/main.c
@@ -8,36 +8,21 @@ void calculo_media(void){
 }
 
 void parte_3(void) {
-/* 
-O sistema deverá conter uma opção para calcular a derivada de uma função simples do tipo:
-f(x) = ax² + bx + c
-O programa deverá receber os coeficientes:
-a
-b
-c
-E apresentar a derivada correspondente:
-f'(x) = 2ax + b
-Exemplo
-Entrada:
-a = 2
-b = 3
-c = 1
-Saída:
-f'(x) = 4x + 3
-*/
-
 int a, b, c;
 
 printf("a: \n");
 scanf("%d", &a);
 
 printf("b: \n");
-scanf("%d", &a);
+scanf("%d", &b);
 
 printf("c: \n");
-scanf("%d", &a);
+scanf("%d", &c);
 
-int derivada = (2 * a * x) + b;
+if (b >= 0)
+    print("f'(x) = %dx + b", (2 * a), b);
+else
+    print("f'(x) = %dx - b", (2 * a), -b);
 }
 
 int main() {
