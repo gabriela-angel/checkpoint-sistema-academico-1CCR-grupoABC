2be5bff8c20bd7b03bdaa6b6b391eb3490ec1533
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 21:30:57 -0300

    Adicionando comentários das novas alterações

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,6 +1,7 @@
 //Alteramos as aspas pelas setas no include
 #include <stdio.h>
 
+//Colocamos os pontos e vírgulas onde estava faltando
 int main() {
     float nota1, nota2, media;
 
@@ -9,10 +10,10 @@ int main() {
 
     printf("Digite a segunda nota: ");
     scanf("%f", nota2);
-
-    media = nota1 + nota2 / 2;
-
-    if (media = 6) {
+//Parênteses nas variáveis de soma
+    media = (nota1 + nota2) / 2;
+//Arrumamos as condições
+    if (5 <= media <= 10) {
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
